c6ccfb171d86cbf2d601ba6c9c89a6059e208c3d
Shaddadi <sheldon.uestc@gmail.com>
2018-11-25 15:36:14 -0600

    add config_info example

--- src/plugins/collectd_config/io.py
+++ src/plugins/collectd_config/io.py
@@ -11,8 +11,34 @@ def read_from_config(config_str):
     return {}
 
 if __name__ == '__main__':
+    setting = {}
+    setting['ReportByCpu'] = 'false'
+    setting['ReportByState'] = 'true'
+    setting['ValuesPercentage'] = 'false'
 
-    x = write_to_config({})
+    cpu1 = {}
+    cpu1['cpu1'] = setting
+
+    setting = {}
+    setting['ReportByCpu'] = 'false'
+    setting['ReportByState'] = 'true'
+    setting['ValuesPercentage'] = 'false'
+
+    cpu2 = {}
+    cpu2['cpu2'] = setting
+
+    setting = {}
+    setting['ReportByCpu'] = 'false'
+    setting['ReportByState'] = 'true'
+    setting['ValuesPercentage'] = 'false'
+
+    cpu3 = {}
+    cpu3['cpu3'] = setting
+
+    config_list = [cpu1, cpu2, cpu3]
+
+    x = write_to_config(config_list)
+    print(x)
 
     y = read_from_config('')
 
